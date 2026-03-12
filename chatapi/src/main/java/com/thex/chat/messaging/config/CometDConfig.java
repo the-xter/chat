69ef091b84c5eab94c86abc2bca7419b58f81621
the-xter <test63845@gmail.com>
2026-03-12 17:43:49 +0700

    Enable Jetty-native WebSocket transport for CometD
    Add jetty-ee10-websocket-jetty-server dependency and configure the
    WebSocket container on the ServletContextHandler before server start.
    Explicitly register JettyWebSocketTransport and provide Jetty Context
    so CometD can set up WebSocket endpoints.

--- chatapi/src/main/java/com/thex/chat/messaging/config/CometDConfig.java
+++ chatapi/src/main/java/com/thex/chat/messaging/config/CometDConfig.java
@@ -5,6 +5,11 @@ import com.thex.chat.messaging.chat.VisitorService;
 import org.cometd.bayeux.server.BayeuxServer;
 import org.cometd.server.BayeuxServerImpl;
 import org.cometd.server.http.jakarta.CometDServlet;
+import org.eclipse.jetty.ee10.servlet.ServletContextHandler;
+import org.eclipse.jetty.ee10.websocket.server.config.JettyWebSocketServletContainerInitializer;
+import org.eclipse.jetty.server.Handler;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -13,9 +18,25 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class CometDConfig {
 
+    @Bean
+    public WebServerFactoryCustomizer<JettyServletWebServerFactory> jettyWebSocketCustomizer() {
+        return factory -> factory.addServerCustomizers(server -> {
+            Handler handler = server.getHandler();
+            while (handler instanceof Handler.Wrapper wrapper) {
+                if (wrapper instanceof ServletContextHandler sch) {
+                    JettyWebSocketServletContainerInitializer.configure(sch, null);
+                    return;
+                }
+                handler = wrapper.getHandler();
+            }
+        });
+    }
+
     @Bean(destroyMethod = "stop")
     public BayeuxServerImpl bayeuxServer(JwtHandshakePolicy jwtHandshakePolicy) {
         BayeuxServerImpl bayeuxServer = new BayeuxServerImpl();
+        bayeuxServer.setOption(BayeuxServerImpl.TRANSPORTS_OPTION,
+                "org.cometd.server.websocket.jetty.JettyWebSocketTransport");
         bayeuxServer.setOption("ws.cometdURLMapping", "/cometd/*");
         bayeuxServer.setSecurityPolicy(jwtHandshakePolicy);
         return bayeuxServer;
@@ -27,6 +48,9 @@ public class CometDConfig {
         return servletContext -> {
             bayeuxServer.setOption(
                     jakarta.servlet.ServletContext.class.getName(), servletContext);
+            var handler = ServletContextHandler.getServletContextHandler(servletContext);
+            bayeuxServer.setOption(
+                    org.eclipse.jetty.server.Context.class.getName(), handler.getContext());
             servletContext.setAttribute(BayeuxServer.ATTRIBUTE, bayeuxServer);
             try {
                 bayeuxServer.start();
